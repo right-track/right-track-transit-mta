ac0a3b0babd944dd3ddf60d7f6edb29fca204ade
David Waring <djw64@cornell.edu>
2021-11-19 22:12:06 -0500

    Update Feed to use new MTA API

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 const path = require('path');
 const RightTrackTransitAgency = require('right-track-transit');
 const feed = require('./feed.js');
-const props = require('../agency.json');
-const propsDir = path.dirname(require.resolve('../agency.json'));
+
+const moduleDirectory = __dirname + "/../";
 
 /**
  * `RightTrackTransitAgency` implementation for the
@@ -22,7 +22,7 @@ class MTA extends RightTrackTransitAgency {
    * Create a `RightTrackTransitAgency` for the MTA
    */
   constructor() {
-    super(propsDir);
+    super(path.normalize(moduleDirectory));
   }
 
   /**
@@ -34,49 +34,7 @@ class MTA extends RightTrackTransitAgency {
    * @param {TransitFeed} [callback.feed] The built Transit Feed for the MTA
    */
   loadFeed(callback) {
-    feed(callback);
-  }
-
-  /**
-   * Get the local absolute path to the icon for the specified division
-   * @param {string} division Transit Division Code
-   * @returns {string|undefined}
-   */
-  getDivisionIconPath(division) {
-    for ( let i = 0; i < props.divisions.length; i++ ) {
-      if ( props.divisions[i].code === division ) {
-        return path.normalize(propsDir + '/' + props.divisions[i].icon);
-      }
-    }
-    return undefined;
-  }
-
-  /**
-   * Get the background color for the specified line
-   * @param {string} line Transit Line Code
-   * @returns {string}
-   */
-  getLineBackgroundColor(line) {
-    for ( let j = 0; j < props.lines.length; j++ ) {
-      if ( props.lines[j].code === line ) {
-        return props.lines[j].backgroundColor;
-      }
-    }
-    return props.lines[0].backgroundColor;
-  }
-
-  /**
-   * Get the text color for the specified line
-   * @param {string} line Transit Line Code
-   * @returns {string}
-   */
-  getLineTextColor(line) {
-    for ( let j = 0; j < props.lines.length; j++ ) {
-      if ( props.lines[j].code === line ) {
-        return props.lines[j].textColor;
-      }
-    }
-    return props.lines[0].textColor;
+    feed(this.config, callback);
   }
 
 }
